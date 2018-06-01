77b8ad27e5c66bd8b86ada669c10c175b8130e8f
Mike McAllister <mike@logikbox.com>
2018-06-01 09:23:14 -0400

    location service works

--- js/common.js
+++ js/common.js
@@ -2,29 +2,15 @@ const apiKey = "6MVMLK-EJ3FXU-BTVB3F-3TNQ";
 const GMAP = "AIzaSyDpn5_1gAS4SJOuzvkeMZK22yOlvZY2kKE";
 const satURL = "https://www.n2yo.com/rest/v1/satellite/"
     
-    // Constructor function for Satelite objects
-    //function Satelite( id, satname,intDesignator,launchDate,satlat,satlng, satalt) {
-    //    this.id = id;
-    //    this.satname = satname;
-    //    this.intDesignator = intDesignator;
-    //    this.launchDate = launchDate;
-    //    this.satlat = satlat;
-    //    this.satlng = satlng;
-    //    this.satalt = satalt;
-    //    
-    //    this.changeName = function (name) {
-    //        this.satname = name;
-    //    }; 
-    //    this.SatDesignation = function () {
-    //      return this.satname + " (" + this.id + ")";
-    //      
-    //    };
-    // 
-    //}
-        function Satelite( id, satname) {
+     //Constructor function for Satelite objects
+    function Satelite( id, satname,intDesignator = "",launchDate = "" ,satlat = "",satlng = "", satalt = "") {
         this.id = id;
         this.satname = satname;
-      
+        this.intDesignator = intDesignator;
+        this.launchDate = launchDate;
+        this.satlat = satlat;
+        this.satlng = satlng;
+        this.satalt = satalt;
         
         this.changeName = function (name) {
             this.satname = name;
@@ -35,6 +21,13 @@ const satURL = "https://www.n2yo.com/rest/v1/satellite/"
         };
      
     }
+    
+    function setLocation (lat = '40.079', lng=  '-75.160', alt = '0'){
+        this.setLat =  lat;
+        this.setLng = lng;
+        this.setAlt = alt;
+    }
+ 
     
   
     
@@ -58,4 +51,22 @@ const satURL = "https://www.n2yo.com/rest/v1/satellite/"
         
     }
     
- 
+    var x = document.getElementById("local");
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);  
+        } else { 
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+    
+    function showPosition(position) {
+        currentLat = position.coords.latitude;
+        currentLng = position.coords.longitude;
+        currentAlt = position.coords.altitude;
+        console.log(currentLat, currentLng, currentAlt);
+        x.innerHTML =  "Lat: " + currentLat + "<br/>Lng: " + currentLng + "<br/>Alt: " + currentAlt;
+        return setLocation(currentLat,currentLng,currentAlt);
+    }
+        

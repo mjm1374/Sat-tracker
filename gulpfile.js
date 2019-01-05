8ef6027aaddc29ed0f7285001d4ecd35d34265fe
Mike McAllister <mike@logikbox.com>
2019-01-04 22:09:04 -0500

    sass build tools in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,22 @@ var cssnano = require('gulp-cssnano');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify-es').default;
+var sourcemaps =  require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var sassdoc = require('sassdoc');
 
 gulp.task('sass', function () {
     return gulp.src('scss/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer())
         .pipe(cssnano())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(sassdoc())
+        // Release the pressure back and trigger flowing mode (drain)
+        // See: http://sassdoc.com/gulp/#drain-event
+        .resume();
 });
 
 gulp.task('js', function () {
